94edae5745ec87c8739cf60e7622497df6d20622
Pawan Maurya <31837242+MisterMaurya@users.noreply.github.com>
2018-10-23 15:04:52 +0530

    aa

---
+++ airlines_basecode/src/main/java/com/airlines/util/Main.java
@@ -0,0 +1,10 @@
+package com.airlines.util;
+
+import com.airlines.frames.Login;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		// ForgotPassword.forgotWindow();
+		Login.loginFrame();
+	}
+}
